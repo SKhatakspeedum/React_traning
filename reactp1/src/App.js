55e1dbbff029deb3b5cb09dd7d2e5cc9edc7e6fd
SKhatakspeedum <skhatak@speedum.co.in>
2023-03-30 16:34:55 +0530

    Session 2 using props

--- reactp1/src/App.js
+++ reactp1/src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Carousel from './Components/Carousel';
 import Session1 from './Components/Session1';
+import Session2 from './Components/Session2';
+
 function App() {
   return (
     <>
    <Navbar></Navbar>
-   <Carousel></Carousel>
    <Session1 name="First"></Session1>
-   
+   <Carousel></Carousel>
+   <Session1 name="Second"></Session1>
+   <Session2 num="5156" heading="props1 heading"></Session2>
+   <Session2 num="5896" heading="props2 heading"></Session2>
+   <Session2 num="9534" heading="props3 heading"></Session2>
 </>
   );
 }
---
+++ reactp1/src/Components/Session2.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Session2(props) {
+  return (
+    <div className="img-4text">
+    <div className="row">
+      <div className="col-3 text">
+        <h4>{props.num}</h4>
+        <h5>{props.heading}</h5>
+        <span className="line mt-4"></span>
+      </div>
+      {/* <div className="col-3 text">
+        <h4>8000</h4>
+        <h5>Lorem, ipsum dolor.</h5>
+        <span className="line mt-4"></span>
+      </div>
+      <div className="col-3 text">
+        <h4>9550</h4>
+        <h5>Lorem, ipsum dolor.</h5>
+        <span className="line mt-4"></span>
+      </div>
+      <div className="col-3 text">
+        <h4>9950</h4>
+        <h5>Lorem, ipsum dolor.</h5>
+        <span className="line mt-4"></span>
+      </div> */}
+    </div>
+  </div>
+  )
+}
