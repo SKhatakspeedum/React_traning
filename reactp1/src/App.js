8aaadb2297b18eaafab362c3a9bd062ddc2dcae6
SKhatakspeedum <skhatak@speedum.co.in>
2023-03-30 15:42:49 +0530

    my_first_prop

--- reactp1/src/App.js
+++ reactp1/src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
 import Navbar from './Components/Navbar';
+import Carousel from './Components/Carousel';
+import Session1 from './Components/Session1';
 function App() {
   return (
     <>
    <Navbar></Navbar>
+   <Carousel></Carousel>
+   <Session1 name="First"></Session1>
+   
 </>
   );
 }
---
+++ reactp1/src/Components/Session1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Session1(props) {
+  return (
+    <div>
+        <h1> My {props.name} heading</h1>
+    </div>
+  )
+}
+
+
